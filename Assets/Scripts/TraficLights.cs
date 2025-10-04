429cb3db42fec957c7293372e6a95af5549a8574
e6494990-maker <e6494990@gmail.com>
2025-10-04 10:18:40 +0100

    Fix update
    Fixed Traffic Light color Changing

--- Assets/Scripts/TraficLights.cs
+++ Assets/Scripts/TraficLights.cs
@@ -12,8 +12,20 @@ public class TraficLights : MonoBehaviour
 
     IEnumerator traficlighting()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(2);
 
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].materials[0].color = Color.red;
+            lights[i].materials[0].SetColor("_EmissionColor", Color.red);
+        }
+        yield return new WaitForSeconds(2);
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].materials[0].color = Color.yellow;
+            lights[i].materials[0].SetColor("_EmissionColor", Color.yellow);
+        }
+        yield return new WaitForSeconds(2);
         for (int i = 0; i < lights.Length; i++)
         {
             lights[i].materials[0].color = Color.green;
